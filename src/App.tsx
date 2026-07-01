b247a3e856db2983e66472ae5274dcd7ddd283fc
LordJayanta <techie.jayanta@gmail.com>
2026-07-01 10:08:47 +0530

    feat(router): Implement React Router
    - Install and configure react-router-dom
    - Add home and privacy-policy route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Index from "./pages/home/Index";
+import PrivacyPolicy from "./pages/privacy-policy/PrivacyPolicy";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+  },
+  {
+    path: "/privacy-policy",
+    element: <PrivacyPolicy />,
+  },
+]);
 
 function App() {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
